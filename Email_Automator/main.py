81a7308de7a5c488f05bf5b0552854356eae67e6
Ayaan Mahimwala <ayaanhaider@gmail.com>
2020-06-26 10:42:40 -0500

    Adding testing code to VS

--- Email_Automator/main.py
+++ Email_Automator/main.py
@@ -2,19 +2,51 @@
 #script process: 
 #read inbox -> parse each email -> put email information into excel sheet -> read excel sheet 
 # -> extrapolate information -> send email to customer 
-import smtplib
-import email
-from email.message import EmailMessage
-import xlrd
-import sys
 import imaplib
+import base64
+import os
+import email
+import xlrd
+import openpyxl
+from email.message import EmailMessage
 
-email_clients = {
-    "gmail" : ("smtp.gmail.com", 587),
-    "outlook" : ("smtp-mail.outlook.com", 587),
-    "hotmail" : ("smtp-mail.outlook.com", 587),
-    "yahoo" : ("smtp.mail.yahoo.com", 587)
-}
+def parse_email(email):
+    email_body = email.get_content()
+    email_body = email_body.replace("\n\n", " ")
+    email_body = email_body.replace("\n", " ")
+    email_body = email_body.replace(": ", ":")
+    param_dict = {}
+    key = ""
+    value = ""
+    onValue = False
+    onKey = True
+    onComments = False
+    for character in email_body:
+        if onKey:
+            if character == ':':
+                if key == "Comments":
+                    onComments = True
+                onValue = True
+                onKey = False
+                continue
+            else:
+                key = key + character
+        elif onValue:
+            if not onComments:
+                if character == ' ':
+                    onValue = False
+                    onKey = True
+                    param_dict[key] = value
+                    key = ""
+                    value = ""
+                else:
+                    value = value + character
+            elif email_body.endswith(character):
+                value = value + " "
+            else:
+                value = value + character
+    param_dict[key] = value            
+    return param_dict
 
 imap_clients = {
     "outlook" : ("imap-mail.outlook.com", 993)
@@ -29,100 +61,57 @@ for line in loginFile:
 
 client = connectionInformation["client"]
 
+#Connect and login to outlook mailbox
 mailbox = imaplib.IMAP4_SSL(imap_clients[client][0], imap_clients[client][1])
-
-connection = smtplib.SMTP(email_clients[client][0], email_clients[client][1])
-connection.ehlo()
-connection.starttls()
 print("successful connection to outlook\n")
-
 username = connectionInformation["username"]
 password = connectionInformation["password"]
-
-connection.login(username, password)
 mailbox.login(username, password)
 print("successful login\n")
 
+#Set the current mailbox to search from and start going thru unread emails
+mailbox.select('Inbox')
+#Container to hold unread emails
+unread_emails = []
+type, data = mailbox.search(None, '(UNSEEN)')
+mail_ids = data[0]
+id_list = mail_ids.split()
+temp = data[0].split()
+if type == "OK":
+    for num in temp:
+        typ, data = mailbox.fetch(num, '(RFC822)' )
+        raw_email = data[0][1]
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                #Create new Email Message object and add it to unread_emails array
+                msg = email.message_from_string(response_part[1].decode('utf-8'))
 
-mailbox.select("INBOX")
-unreadEmails=0
-(retcode, messages) = mailbox.search(None, '(UNSEEN)')
-#print("This is being printed by line 47 " + retcode, messages)
-#print(messages[0].split())
-if retcode == 'OK':
-   for num in messages[0].split() :
-      print ('Processing ')
-      unreadEmails=unreadEmails+1
-      typ, data = mailbox.fetch(num,'(RFC822)')
-      #print(data)
-      for response_part in data:
-        if isinstance(response_part, tuple):
-            original = email.message_from_bytes(data[0][1])
-            
-            print (original['From'])
-            print (original['Subject'])
-            if original.is_multipart():
-                for payload in original.get_payload():
-                    print(payload.get_payload())
-            #else:
-                #print(original.get_payload())
-            
-             
-            typ, data = mailbox.store(num,'+FLAGS','\\Seen')
+                unread_email = EmailMessage()
+                unread_email['Subject'] = msg['subject']
+                unread_email['From'] = msg['from']
+                unread_email.set_content(msg.get_payload()[0].get_payload())
 
-print (unreadEmails)
+                unread_emails.append(unread_email)
+print(f"Unread emails fetched:{len(unread_emails)}")
 
-
-connection.quit()
+#Close connection to outlook and logout
 mailbox.close()
 mailbox.logout()
-sys.exit(0)
+print("Closing connection and logging off")
 
-recipient = input("Please enter the email address of the recipient\n")
+workbook = openpyxl.load_workbook("templates.xlsx")
+customers = workbook["Customers"]
 
-workbook = xlrd.open_workbook("templates.xlsx")
-templateSheet = workbook.sheet_by_name("templates")
+#Parse and create param_dict + key_dict
+#Insert information into excel file
+for email in unread_emails:
+    param_dict = parse_email(email)
 
-found = False
-template_name_row = 0
-templateName = input("Please enter the name of the template you want to use or type quit\n")
-while not found:
-    if "quit" in templateName:
-        sys.exit(0)
-    if templateName in templateSheet.col_values(0):
-        template_name_row = templateSheet.col_values(0).index(templateName)
-        found = True
-        break
-    templateName = input(templateName +" was not found, please enter the name of the template you want to use or type quit\n")
+    key_dict = []
+    for row in customers.iter_cols(min_row=1, max_col=customers.max_column, max_row=2, values_only=True):
+        key_dict.append(row[0])
 
-prompt_col = templateSheet.row_values(0).index("Required Prompts")
-
-prompts = templateSheet.cell_value(template_name_row, prompt_col).split(",")
-prompt_dict = {}
-for prompt in prompts:
-    test = input("Please enter value for " + prompt + "\n")
-    prompt_dict[prompt] = test
-
-message_index = templateSheet.row_values(0).index("Template Message")
-body = templateSheet.cell_value(template_name_row, message_index)
-for item in prompt_dict:
-    body = body.replace(item, prompt_dict[item])
-
-subject_index = templateSheet.row_values(0).index("Subject")
-subject = templateSheet.cell_value(template_name_row, subject_index)
-for item in prompt_dict:
-    subject = subject.replace(item, prompt_dict[item])
-
-
-
-email = EmailMessage()
-email['From'] = username
-email['To'] = recipient
-email['Subject'] = subject
-email.set_content(body)
-
-connection.send_message(email)
-print("Email was successfully sent to ", recipient)
-connection.quit()
-mailbox.close()
-mailbox.logout()
+    new_customer_row = customers.max_row + 1
+    for current_col in range(customers.max_column):
+        if key_dict[current_col] in param_dict:
+            customers.cell(row=new_customer_row, column=current_col + 1).value = param_dict[key_dict[current_col]]
